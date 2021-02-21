1e4ce110f18b46b505c17bdbae7b44a75b81c051
Andrew Cho <ac5bg@virginia.edu>
2021-02-21 15:34:56 -0500

    changed settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
+try:
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
